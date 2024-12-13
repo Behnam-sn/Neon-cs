742313ef9feb81bceac3904e0381aa7cdaec5305
Behnam <behnam.dev@outlook.com>
2024-12-14 01:54:59 +0330

    Update Product.cs

--- src/Neon.Domain/Entities/Product.cs
+++ src/Neon.Domain/Entities/Product.cs
@@ -4,5 +4,7 @@ namespace Neon.Domain.Entities;
 
 public sealed class Product : Entity
 {
+    public string Title { get; private set; }
+    public string Description { get; private set; }
     public List<ShopProductAssignment> ShopProductAssignments { get; private set; }
 }
