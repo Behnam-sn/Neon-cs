bd0e71e7a4676976b4b91c0a25a541d697adc3b2
Behnam <behnam.dev@outlook.com>
2024-12-13 17:19:27 +0330

    Create Customer.cs

---
+++ Neon.Domain/Entities/Users/Customer.cs
@@ -0,0 +1,10 @@
+using Neon.Domain.Entities.Carts;
+
+namespace Neon.Domain.Entities.Users;
+
+public sealed class Customer : User
+{
+    public List<Address> Addresses { get; private set; }
+    public List<Cart> Carts { get; private set; }
+    public List<Payment> Payments { get; private set; }
+}
