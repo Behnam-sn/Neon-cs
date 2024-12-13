20133d6b4bb5b124257ea33794027e4273a22573
Behnam <behnam.dev@outlook.com>
2024-12-13 23:19:23 +0330

    Update Entity.cs

--- Neon.Domain/Abstractions/Entity.cs
+++ Neon.Domain/Abstractions/Entity.cs
@@ -3,4 +3,6 @@ namespace Neon.Domain.Abstractions;
 public abstract class Entity
 {
     public Guid Id { get; private init; }
+    public DateTime CreatedAt { get; private init; }
+    public DateTime ModifiedAt { get; private init; }
 }
