91a6ce987a7ec126f00f983819e6c24b34862b6b
Behnam <behnam.dev@outlook.com>
2024-12-13 17:37:23 +0330

    Update ShopProductAssignment.cs

--- Neon.Domain/Entities/ShopProductAssignment.cs
+++ Neon.Domain/Entities/ShopProductAssignment.cs
@@ -1,6 +1,5 @@
 using Neon.Domain.Abstractions;
 using Neon.Domain.Entities.Places;
-using Neon.Domain.Entities.Users;
 
 namespace Neon.Domain.Entities;
 
@@ -10,5 +9,4 @@ public sealed class ShopProductAssignment : Entity
     public Product Product { get; private set; }
     public decimal Price { get; private set; }
     public long Quantity { get; private set; }
-    public Vendor Owner { get; private set; }
 }
