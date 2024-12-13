257dfbde1a6b9502d060ac16b6e9fc956ae19641
Behnam <behnam.dev@outlook.com>
2024-12-13 17:37:29 +0330

    Update User.cs

--- Neon.Domain/Entities/Users/User.cs
+++ Neon.Domain/Entities/Users/User.cs
@@ -8,5 +8,5 @@ public abstract class User : Entity
     public string FirstName { get; private set; }
     public string LastName { get; private set; }
     public Email Email { get; private set; }
-    public MobilePhoneNumber MobilePhoneNumber { get; set; }
+    public MobilePhoneNumber MobilePhoneNumber { get; private set; }
 }
