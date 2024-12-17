28a0cc190af3f433f60091841c0e267219ec5417
Behnam <behnam.dev@outlook.com>
2024-12-18 00:27:09 +0330

    Update Product

--- src/Neon/Domain/Products/Entities/Product.cs
+++ src/Neon/Domain/Products/Entities/Product.cs
@@ -12,15 +12,20 @@ public sealed class Product : Entity
 {
     public string Title { get; }
     public string Description { get; }
-    public float AverageRate { get; }
-    public int ReviewsCount { get; }
     public string Slug { get; }
+    public Guid CategoryId { get; }
+
+    // Navigations
     public Category Category { get; }
     public List<CategoryAttributeValue> CategoryAttributeValues { get; }
     public List<ProductAttribute> ProductAttributes { get; }
-    public List<object> Images { get; }
     public List<Tag> Tags { get; }
     public List<Review> Reviews { get; }
-    public List<Product> RelatedProducts { get; }
     public List<ShopProduct> ShopProducts { get; }
+
+    // Extras
+    public float AverageRate { get; }
+    public int ReviewsCount { get; }
+    public List<Product> RelatedProducts { get; }
+    public List<object> Images { get; }
 }
