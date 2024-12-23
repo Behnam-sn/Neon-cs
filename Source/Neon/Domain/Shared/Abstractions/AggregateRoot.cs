0f6a911be563a698fe0652493e635cf96ec40717
Behnam <behnam.dev@outlook.com>
2024-12-24 00:57:19 +0330

    Update AggregateRoot

--- Source/Neon/Domain/Shared/Abstractions/AggregateRoot.cs
+++ Source/Neon/Domain/Shared/Abstractions/AggregateRoot.cs
@@ -1,10 +1,20 @@
 namespace Neon.Domain.Shared.Abstractions;
 
-public abstract class AggregateRoot : Entity
+public abstract class AggregateRoot<TId> : Entity<TId>
 {
     private readonly List<IDomainEvent> _domainEvents = [];
 
     public IEnumerable<IDomainEvent> DomainEvents => _domainEvents;
+
+    protected AggregateRoot()
+    {
+
+    }
+
+    protected AggregateRoot(TId id) : base(id)
+    {
+
+    }
 
     protected void RaiseDomainEvent(IDomainEvent domainEvent)
     {
